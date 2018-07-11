d870e15a3e1e845f12604dee8788e080f6bb1e72
Leonardo <quenixserra@gmail.com>
2018-07-11 19:40:03 -0300

    Teste

---
+++ survey_senac/troll.php
@@ -0,0 +1,40 @@
+<html>	
+	<body>
+		<head>
+			<meta charset = "UTF-8">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		</head>
+		<body>
+		
+		<style>
+		
+		.logo{
+			width: 350px;
+			position: absolute;
+			top: 5%;
+		}
+					.block{
+				position: absolute;
+				font-size: 80px;
+				color: #005594;	
+				top: 10%;
+				left: 5%;
+				font-family: "arial";
+				margin-top:270px;
+		
+		</style>
+		
+		
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img src="img/ninja.png" class="logo">
+					</div>
+					<div class="col-md-offset-1 col-md-8">
+						<h1 class="block">Página bloqueada!</h1>
+					</div>
+				</div>
+			</div>
+		</body>
+</html>				
